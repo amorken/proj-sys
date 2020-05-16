5632bd3890cc003c28e486e27c72b72bcc29c49b
Stephan Hügel <shugel@tcd.ie>
2020-05-16 23:55:05 +0100

    Depend on CXX env var for c++ compiler

--- build.rs
+++ build.rs
@@ -138,7 +138,7 @@ fn main() {
     config.define("BUILD_SHARED_LIBS", "OFF");
     config.define("BUILD_TESTING", "OFF");
     config.define("CMAKE_BUILD_TYPE", "Release");
-    config.define("CMAKE_CXX_COMPILER", "clang++-7");
+    // config.define("CMAKE_CXX_COMPILER", "clang++-7");
     // config.define("CMAKE_C_FLAGS", "pic");
     // config.define("CMAKE_CXX_FLAGS", "pic");
     // if target.contains("linux") {
