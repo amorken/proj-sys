e905b177b23c5f2e6ae3cb1365d7a4cc5d45e31f
Stephan Hügel <shugel@tcd.ie>
2020-05-16 23:04:18 +0100

    Try to fix GLIBCXX_USE_CXX11_ABI flag

--- build.rs
+++ build.rs
@@ -139,7 +139,7 @@ fn main() {
     config.define("BUILD_TESTING", "OFF");
     config.define("CMAKE_BUILD_TYPE", "Release");
     config.define("CMAKE_CXX_COMPILER", "g++-7");
-    config.define("CMAKE_CXX_FLAGS", "GLIBCXX_USE_CXX11_ABI=0");
+    config.define("CMAKE_CXX_FLAGS", "_GLIBCXX_USE_CXX11_ABI=0");
     // if target.contains("linux") {
     //     config.define("CMAKE_CXX_COMPILER", env::var("CXX").unwrap());
     // }
