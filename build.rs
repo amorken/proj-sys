4992baacc4d780e684cdf19f5c11700bb4ab4bb5
Stephan Hügel <shugel@tcd.ie>
2020-05-22 14:33:59 +0100

    See what's in the libstdc++ dir

--- build.rs
+++ build.rs
@@ -139,7 +139,8 @@ fn main() {
     println!("cargo:rustc-link-lib=dylib=curl");
     println!("cargo:rustc-link-lib=dylib=tiff");
     if target.contains("linux") {
-        println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
+        // println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
+        println!("cargo:rustc-link-search=/usr/lib/gcc/i686-linux-gnu/9/");
         println!("cargo:rustc-link-lib=stdc++");
     }
     if target.contains("apple") {
